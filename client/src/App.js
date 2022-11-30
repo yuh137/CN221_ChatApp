cb18743bc365b5541bb0a6394aa65fbf00887609
nqhuy137 <nquochuy137@gmail.com>
2022-11-30 23:45:13 +0700

    switch to another ytber

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import Topbar from "./components/Topbar/Topbar";
+import Navigation from "./components/Navigation";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar></Topbar>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/login" element = {<Logingin />} />
+        <Route path = "/signup" element = {<Signupgnup />} />
+        <Route path = "/chat" element = {<Chatat />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/messenger/Messenger.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Messenger = () => {
-  return (
-    <div>Messenger</div>
-  )
-}
-
-export default Messenger
