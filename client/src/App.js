421be7a88421547c67b502e6399e57d7b2082fe0
nqhuy137 <nquochuy137@gmail.com>
2022-12-01 00:39:29 +0700

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
@@ -11,9 +11,9 @@ function App() {
       <Navigation />
       <Routes>
         <Route path = "/" element = {<Home />} />
-        <Route path = "/login" element = {<Logingin />} />
-        <Route path = "/signup" element = {<Signupgnup />} />
-        <Route path = "/chat" element = {<Chatat />} />
+        <Route path = "/login" element = {<Login />} />
+        <Route path = "/signup" element = {<Signup />} />
+        <Route path = "/chat" element = {<Chat />} />
       </Routes>
     </Router>
   );
